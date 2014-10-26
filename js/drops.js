9cb2ff3699ee31e292b1d1942ba5d028fdc58f57
rbtdev <rob@rbtdev.com>
2014-10-26 07:51:06 -0700

    Initial commit

---
+++ js/drops.js
@@ -0,0 +1,340 @@
+
+var drops = {
+	areas:[
+		{
+		name: "Sorento Valley",
+		locations: [
+			{
+			name: "Active Network",
+			address: "10182 Telesis Court, Suite 100, San Diego, CA 92121",
+			note: "",
+			phone: "(858) 964-3800",
+			hours: "M-F 9am - 5pm",
+			incentive: "",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Downtown San Diego", 
+		locations:[
+			{
+			name: "McFaddens  Restaurant", 
+			address: "731 5th Ave, San Diego, CA 92101",
+			note: "",
+			phone: "(619) 795-2500", 
+			hours: "Sunday 10:00am - 2:00am",
+			incentive: "Free Appetizer with Donation of 2 Blankets!",
+			website: "www.mcfaddenssandiego.com"
+			},
+			{
+			name: "Big Bricks Training",
+			address: "740 13th St #503, San Diego, CA 92101",
+			note: "",
+			phone: "(619) 566-6820",
+			hours: "DROP OFF TIMES: M-F 5-7pm Sat 8-11am",
+			incentive: "",
+			website: "www.bigbrickstraining.com"
+			}
+		]
+		},
+		{
+		name: "South Bay",
+		locations: [
+			{
+			name: "Cafe LaMaze",
+			address: "1133 13th St., Imperial Beach, CA 91932",
+			note: "",
+			phone: "(619) 863-5353",
+			hours: "Sunday 10am-2am",
+			incentive: "Free Appetizer with Donation of 2 Blankets!",
+			website: "www.cafelamaze.com"
+			},
+			{
+			name: "Office Lounge",
+			address: "1133 13th St, Imperial Beach, CA 91932",
+			note: "",
+			phone:"(619) 863-5353",
+			hours: "Sunday 10:00am - 2:00am",		
+			incentive: "",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Pacific Beach",
+		locations: [
+			{
+			name:"Living Waters Wellness and Spa",
+			address: "3517 Del Rey St #101, San Diego, CA 92109",
+			note: "",
+			phone: "(619) 224-1962",
+			hours: "CALL FOR DROP OFF TIMES",
+			incentive: "Donate a blanket get a $20 gift certificate!",
+			website: "www.livingwaterrejuvenation.com/sandiego"
+			},
+			{
+			name: "Hollywood Tans",
+			address: "1321 Garnet Ave, San Diego, CA 92109",
+			note: "",
+			phone: "(858) 490-0555",	
+			hours: "Mon-Fri 9am-9pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Mission Valley",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "5171 Mission Center Rd, San Diego, CA 92108",
+			note: "Behind Best Buy",
+			phone: "(858) 260-9017",
+			hours: "Mon-Fri 8am-9pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			},
+{
+			name: "Art Institute of California, San Diego",
+			address: "7650 Mission Valley Road, San Diego, CA 92108-4423",
+			note: "",
+			phone: "(858) 598-1399",
+			hours: "Mon-Fri 8am-6pm",
+			incentive: "",
+			website: "artinstitutes.edu"
+			}
+		]
+		},
+		{
+		name: "Carmel Mountain Ranch",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "11467 Carmel Mountain Rd, San Diego, CA 92128",
+			note: "",
+			phone: "(858) 487-8787",
+			hours: "Mon- Fri 9am-9pm, Sat 10am-7pm, Sun 10am-6pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Hillcrest",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "500 University Ave, San Diego, CA 92103",
+			note: "Corner of 5th & University",
+			phone: "(619) 220-8444",
+			hours: "Mon-Fri 9am-8pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Point Loma Area",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "3589 Midway Drive, San Diego, CA 92110",
+			note: "Next to Broken Yolk",
+			phone: "(619) 222-2200",
+			hours: "Mon-Fri 9pm-9pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			},
+			{
+			name: "Paradise Lounge & Grill",
+			address: "2732 Midway Dr, San Diego, CA 92110",
+			note: "",
+			phone: "(619) 225-8110",
+			hours: "8:00am - 2:00am",
+			incentive: "",
+			website: ""
+			},
+			{
+			name: "Pacers",
+			address: "3334 Midway Drive, San Diego, CA 92110",
+			note: "",
+			phone: "(619) 222-2000",
+			hours: "Mon-Sun 11:30am-2am",
+			incentive: "",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "East County",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "163 Fletcher Parkway, El Cajon, CA 92020",
+			note: "Parkway Mall outside next to Borders bookstore",
+			phone: "(619) 440-8900",
+			hours: "Mon-Fri 9am-8pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			},
+			{
+			name: "P.S.A.R",
+			address: "1150 Broadway, El Cajon, CA 92021",
+			note: "",
+			phone: "(619) 579-0333",
+			hours: "M-F 8-5",
+			incentive: "",
+			website: ""
+			},
+			{
+			name: "Brinegar Chiropractic & Massage",
+			address: "8760 Cuyamaca St, Suite 203, Santee, CA 92071",
+			note: "",
+			phone: "(619) 258-1011",
+			hours: "MWF 9am -5pm, T/Th 2pm - 5pm, Sat 9am-12pm",
+			incentive:"Bring a Blanket and Receive a 45 Minute Massage for $20.00 Or 1 hour for 25.00",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Mira Mesa",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "8118 Mira Mesa Boulevard, San Diego, CA 92126",
+			note: "Next to Panda Express",
+			phone: "(619) 566-0078",
+			hours: "Mon-Fri 9am-8pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "4S Ranch / Rancho Bernardo",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "10449 Reserve Dr, San Diego, CA 92127",
+			note: "4S Common Shopping Center",
+			phone: "858-451-8267",
+			hours: "Mon-Fri 9am-8pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "Oceanside",
+		locations: [
+			{
+			name: "Hollywood Tans",
+			address: "2484 Vista Way, Oceanside, CA 92054",
+			note: "Next to Trader Joe's",
+			phone: "(760) 966-1494",
+			hours: "Mon-Fri 9am-8pm, Sat 9am-6pm, Sun 10am-5pm",
+			incentive: "One Free Level 4 UV Tan OR $10 Mystic With Donation!",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "La Mesa",
+		locations: [
+			{
+			name: "imortgage",
+			address: "7777 Alvarado Road Suite 701, La Mesa, CA 91941",
+			note: "",
+			phone: "(619) 719-0555",
+			hours: "Mon-Fri 8am-5pm",
+			incentive: "",
+			website: ""
+			},
+			{
+			name: "Windermere Real Estate",
+			address: "8277 La Mesa Boulevard, La Mesa, CA 91942",
+			note: "La Mesa Village",
+			phone: "(619) 303-9500",
+			hours: "",
+			incentive:"",
+			website: ""
+			}
+		]
+		},
+		{
+		name: "North County",
+		locations: [
+			{
+			name: "Cavallo MotorSports",
+			address: "6010 Avenida Encinas, Carlsbad, CA 92011",
+			note: "",
+			phone: "",
+			hours: "M-F 7:30am - 7pm",
+			incentive:"",
+			website: "www.cavallomotorsports.com"
+			}
+		]
+		}
+	].sort(
+		function (a,b) {
+			if (a.name < b.name)
+				return -1;
+			if (a.name > b.name)
+				return 1;
+			return 0;
+		}
+	)
+};
+
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+function locationList() {
+
+	for (var i=0; i<drops.areas.length; i++) {
+		document.write('<p class = "area">' + drops.areas[i].name + '</p>');
+		for (var j=0; j<drops.areas[i].locations.length; j++) {
+			document.write('<p class = "location"><a href = javascript:loadDetailsPage(' + i + ',' + j + ');>' + drops.areas[i].locations[j].name + '</a>');
+			if(drops.areas[i].locations[j].incentive != "") {
+				document.write('<br><span class = "dropIncentive">' + drops.areas[i].locations[j].incentive + '</span></br></p>');
+			}
+			else {
+				document.write('</p>');
+			}
+		}
+	}
+}
+
+function loadDetailsPage(area, location) {
+	window.location = "locationDetails.html?area=" + area + "&location=" + location;
+}
+
+function writeDetail(className, detail) {
+	if (detail != "") {
+		document.write("<p class = '" + className + "'>" + detail + "</p>");
+	}
+}
+
+function locationDetails(area, location) {
+
+	var drop = drops.areas[area].locations[location];
+	writeDetail("dropName", drop.name);
+	writeDetail("dropAddress", drop.address);
+	writeDetail("dropNote", drop.note);
+	writeDetail("dropPhone", drop.phone);
+	writeDetail("dropHours", drop.hours);
+	writeDetail("dropIncentive",drop.incentive);
+	writeDetail("dropUrl", "<a href = http://" + drop.website + ">"+ drop.website + "</a>");
+}
