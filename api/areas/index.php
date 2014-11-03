913eb2d8fa656eb176a5d63afb45993d9150dae3
Rob Thuleen <rob.thuleen@gmail.com>
2014-11-03 08:31:02 -0800

    Api updates

--- api/areas/index.php
+++ api/areas/index.php
@@ -1,17 +1,12 @@
 <?php
- 
-//Connect to the Database
 $con = mysql_connect('payattention.db.11463497.hostedresource.com', 'payattention', 'Rip1!tide') or die ('Connection Error.');
 mysql_select_db('payattention', $con) or die('MySQL Error.');
- 
-//Run our query
-$result = mysql_query('SELECT * from locations');
-$encode = array();
-
+$result = mysql_query('SELECT * FROM areas', $con) or die('SQL Error.');
+$resultArray = array();
 while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-   $encode[] = $row;
+   $resultArray[] = $row;
 }
-$output = json_encode(array('locations' => $encode));
+$output = json_encode(array('areas' => $resultArray));
 //Output the output.
 echo $output;
  
