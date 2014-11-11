b7efbfdcf57ec331c470d1c140429ea2a5f8bf33
Rob Thuleen <rob.thuleen@gmail.com>
2014-11-11 00:33:58 -0800

    Updates

--- server/mocks/areas.js
+++ server/mocks/areas.js
@@ -6,13 +6,17 @@ module.exports = function(app) {
 
   areasRouter.get('/', function(req, res) {
   	Area.find({}, function(err, areas) {
-           res.status(200).send({"areas":areas});
+          return res.status(200).send({"areas":areas});
     });
   });
 
   areasRouter.get('/:id', function(req, res) {
-    Area.find({_id: req.params.id}, function(err, area) {
-           res.status(200).send({"area":area});
+    Area.findOne({_id: req.params.id}, function(err, area) {
+        var status = 200;
+        if (!area) {
+          var status = 404;
+        }
+        return res.status(status).send({"area":area});
     });
   });
 
@@ -40,19 +44,22 @@ module.exports = function(app) {
     console.log('Req: ' + JSON.stringify(req.body))
     Area.findById(req.params.id, function (err, area) {
       if (!err) {
+        if (!area) {
+          return res.status(404).send({});
+        }
         area.name = req.body.area.name;
         area.locations = req.body.area.locations
         area.save(function (err, area) {
           if (!err) {
-            res.status(200).send({area: area})
+            return res.status(200).send({area: area})
           }
           else {
-            res.status(500).send(err)
+            return res.status(500).send(err)
           }
         })  
       }
       else {
-        res.status(500).send(err)
+        return res.status(500).send(err)
       }
     });
   });
